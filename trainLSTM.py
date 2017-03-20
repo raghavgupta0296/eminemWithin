440bd4c9c15f2a9a9a9d922ce3003db826a579c5
Raghav Gupta <raghav0296@gmail.com>
2017-03-20 22:49:25 +0530

    Add files via upload

--- trainLSTM.py
+++ trainLSTM.py
@@ -7,6 +7,8 @@ import tensorflow as tf
 import numpy as np
 import word2vec2
 
+tf.set_random_seed(1001)
+
 f = open("cleanedRapLyrics.txt")
 data = f.read().lower()
 
@@ -61,11 +63,10 @@ print(" Embeddings Shape : ", embeddings.shape)
 #	print "embeddings : ",sess.run(e)
 
 
-words_to_read = 3
+words_to_read = 20
 
 X = tf.placeholder(tf.float32, shape=(None, words_to_read, embedding_size))
 Y = tf.placeholder(tf.float32, shape=(None,vocab_size))
-
 
 def one_hot(num):
     a = np.zeros(vocab_size)
@@ -87,7 +88,7 @@ def newXY(i):
     frame = data2[(i):(i + words_to_read)]
     output = data2[i + words_to_read]
     # Y = tf.nn.embedding_lookup(embeddings,output)
-    y = one_hot(output)
+    y = one_hot(output) #y = embeddings[output]
     for f in frame:
         # t = tf.nn.embedding_lookup(embeddings,f)
         x = np.append(x, embeddings[f])
@@ -112,11 +113,11 @@ sess = tf.Session()
 K.set_session(sess)
 
 model = Sequential()
-model.add(LSTM(10, input_shape=(int(X.get_shape()[1]), int(X.get_shape()[2])), return_sequences=True))
+model.add(LSTM(256, input_shape=(int(X.get_shape()[1]), int(X.get_shape()[2])), return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(10))
+model.add(LSTM(256, return_sequences=False))
 model.add(Dropout(0.2))
-model.add(Dense(vocab_size, activation="softmax"))
+model.add(Dense(vocab_size)) #, activation="softmax"))
 # model.compile(loss="categorical_crossentropy",optimizer="adam")
 
 # model = LSTM(256,input_shape=(X.get_shape()[1],X.get_shape()[2]),init='uniform',return_sequences=True)(X)
@@ -127,8 +128,10 @@ model.add(Dense(vocab_size, activation="softmax"))
 
 outModel = model(X)
 
-loss = tf.reduce_mean(categorical_crossentropy(Y, outModel))
-train_step = tf.train.AdamOptimizer().minimize(loss)
+# loss = tf.reduce_mean(categorical_crossentropy(Y, outModel))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(outModel,Y))
+
+train_step = tf.train.AdamOptimizer(0.001).minimize(loss)
 
 print("MODEL CREATED..............")
 
@@ -143,51 +146,81 @@ print("Training...................")
 
 
 # model.fit(X,Y,nb_epoch=20,batch_size=128,callbacks=[checkpoint])
-
+'''
+def closestEmbedding(predictedWord):
+    ctrEm=[]
+    for q in range(len(embeddings)):
+        ctr = 0
+        for w in range(len(embeddings[q])):
+            ctr += (predictedWord[0][w] - embeddings[q][w])**2
+        ctrEm.append(ctr)
+    predictedWord = np.argmax(ctrEm)
+    # to not get UNK as prediction
+    if (predictedWord == 0):
+        predictedWord = np.argsort(ctrEm)[-2]
+    #
+    return predictedWord
+'''
 def generateTest():
     words_to_generate = 30
-    tx = ["came", "back", "from"]#, "those", "strange", "streets"]
+    testSeed = np.random.randint(low=1,high=(len(data2)-words_to_read),size=1)
+    testSeed = int(testSeed)
+    tx = []
+    for t in range(testSeed,testSeed+words_to_read,1):
+        tx.append(int2word[data2[t]])
+    actualY = data2[testSeed+words_to_read]
+    actualY = one_hot(int(actualY))
+    actualY = np.reshape(actualY,(1,vocab_size))
+    # tx = ["came", "back", "from", "those", "strange", "streets","\n","any","day","of","those","lame","dreams","\n","move","with","the","same","speed","\n"]
     print(tx)
-    tx = [word2int[tx_i] for tx_i in tx]
+    for i in range(len(tx)):
+        try:
+            tx[i] = word2int[tx[i]]
+        except:
+            tx[i] = word2int['UNK']
+    # tx = [word2int[tx_i] for tx_i in tx]
     tx = [embeddings[tx_i] for tx_i in tx]
     for j in range(words_to_generate):
         tx = np.reshape(tx, (1, words_to_read, embedding_size))
 
+        if (j == 0):
+            print("loss : ", sess.run([loss], feed_dict={X: tx, Y: actualY, K.learning_phase(): 0}))
+
         predictedWord = model.predict(tx)
+        '''
+        predictedWord = closestEmbedding(predictedWord)
+        predictedWord2 = embeddings[predictedWord]
+        predictedWord2 = np.reshape(predictedWord2,[1,embedding_size])
+        '''
 
         predictedWord = np.asarray(predictedWord).astype('float64')
-        # print ("pW : ", predictedWord)
-
         predictedWord = np.reshape(predictedWord, (vocab_size))
-        # print predictedWord, predictedWord.shape
-
         # temperate
-        temperature = 0.4
-        predictedWord = np.log(predictedWord) / temperature
-        pw = np.exp(predictedWord)
-        predictedWord = pw / np.sum(pw)
-
-        # print ("pW2 : ",predictedWord)
-
-        pm = np.random.multinomial(1, predictedWord, 1)
-        # print("pm : ",pm)
-        pm = np.reshape(pm,(vocab_size))
-
+        # temperature = 0.6
+        # predictedWord = np.log(predictedWord) / temperature
+        # pw = np.exp(predictedWord)
+        # predictedWord = pw / np.sum(pw)
+        # pm = np.random.multinomial(1, predictedWord, 1)
+        # pm = np.reshape(pm,(vocab_size))
+        pm = predictedWord # either this or temp
+        pw = np.exp(pm)
+        pm = pw / np.sum(pw)
         predictedWord = np.argmax(pm)
         #
-
         # to not get UNK as prediction
         if (predictedWord==0):
             predictedWord = np.argsort(pm)[-2]
         #
-
+        predictedWord2 = one_hot(predictedWord)
+        predictedWord2 = np.reshape(predictedWord2,(1,vocab_size))
         tx = np.reshape(tx, (words_to_read, embedding_size))
 
         e = embeddings[predictedWord]
         e = np.reshape(e, (1, embedding_size))
         tx = np.append(tx, e, axis=0)
         tx = tx[1:]
-        print(int2word[predictedWord], " ",end='')
+        print(int2word[predictedWord],' ', end='')
+        # tx = np.reshape(tx,(1,words_to_read,embedding_size))
 
 # print (len(data2))  # - 106989 ...106932 106983
 batch_size = int(len(data2)/213)
@@ -195,7 +228,7 @@ batch_size = int(len(data2)/213)
 with sess.as_default():
     sess.run(tf.global_variables_initializer())
     for epoch in range(1, 51):
-        print("Epoch ", epoch, "/500")
+        print("\n\n Epoch ", epoch, "/500")
         for i in range(0,(len(data2) - words_to_read)-batch_size,batch_size):
             x = np.array([])
             y = np.array([])
@@ -208,6 +241,7 @@ with sess.as_default():
             x = np.reshape(x, (batch_size, words_to_read, embedding_size))
             y = np.reshape(y, (batch_size, vocab_size))
             train_step.run(feed_dict={X: x, Y: y, K.learning_phase(): 1})
+            # model.fit(x, y, batch_size=batch_size)
         # if (i%1000==0):
         # test model
         generateTest()
