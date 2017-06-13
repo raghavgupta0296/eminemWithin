459e561db9e9c97bb650e99e826f047788fba1b8
Raghav Gupta <raghav0296@gmail.com>
2017-06-14 00:00:52 +0530

    Add files via upload

--- RapModel.py
+++ RapModel.py
@@ -7,7 +7,17 @@ from keras.layers.normalization import BatchNormalization
 
 f = open("cleanedRapLyrics.txt",'r',encoding='utf-8')
 lyrics = f.read()
-lyrics = lyrics[::-1]
+f.close()
+lines = lyrics.split("\n")
+lyrics = ""
+for l in lines:
+    try:
+        if l[-1] == " ":
+            l = l[:-1]
+    except:
+        pass
+    lyrics = lyrics+l[::-1]+"\n"
+lyrics = lyrics[:-1]
 chars = sorted(set(lyrics))
 char2int = dict(zip(chars,range(len(chars))))
 int2char = dict(zip(char2int.values(),char2int.keys()))
@@ -42,6 +52,8 @@ model.add(Dropout(0.2))
 model.add(Dense(len(chars)))
 model.add(BatchNormalization())
 model.add(Activation('softmax'))
+filepath = "weights-improvement-37-1.2212-bigger.hdf5"
+model.load_weights(filepath)
 model.compile(loss='categorical_crossentropy',optimizer='rmsprop')
 
 filepath="/output/weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
